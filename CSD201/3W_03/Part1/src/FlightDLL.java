07004538948dd48337566c77fb2da4c86f283310
x1ptase <anhtph911@gmail.com>
2025-12-16 22:47:02 +0700

    update part1

--- CSD201/3W_03/Part1/src/FlightDLL.java
+++ CSD201/3W_03/Part1/src/FlightDLL.java
@@ -29,20 +29,52 @@ public class FlightDLL { //Implement Singly Linked List
     
     //1 diem
     void addFirst(Flight f){
-        
+        if(f.getStatus() == -1 || f.getStatus() == 0 || f.getStatus() == -1 && (f.getPrice() > 0)){
+            Node p = new Node(f, null);
+            if(header == null){
+                header = trailer = p;
+            } else{
+                p.next = header;
+                header = p;
+            }
+            size ++;
+        }
     }
     //1 diem
     void addFirst1(String code, String from, String to, int status, double price){
-        
+        if(status == -1 || status == 0 || status == 1 && (price > 0)){
+            Flight flight = new Flight(code, from, to, status, price);
+            Node p = new Node(flight, null);
+            if(header == null){
+                header = trailer = p;
+            } else{
+                p.next = header;
+                header = p;
+            }
+            size++;
+        }
     }
     //1 diem
     void traverse(){
-        
+        Node p = header;
+        while(p != null){
+            System.out.println(p.infor);
+            p = p.next;
+        }
     }
     
     public Node getCheapestFlight(){ //Tim chuyen bay co gia re nhat
-        
-        return null;
+        if(header == null){
+            return null;
+        }
+        Node p = header.next;
+        while(p != null){
+            if(p.infor.getPrice() < header.infor.getPrice()){
+                header = p;
+            }
+            p = p.next;
+        }
+        return header;
     }
     
 }
